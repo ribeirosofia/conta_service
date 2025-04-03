dd8b50888294fefc73455c39406bdb2e1c9886bc
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-02 21:51:16 -0300

    feat: create service app

---
+++ src/main/java/com/bootcamp/conta_service_2025/service/ContaService.java
@@ -0,0 +1,50 @@
+package com.bootcamp.conta_service_2025.service;
+
+import com.bootcamp.conta_service_2025.dto.ContaRequestDTO;
+import com.bootcamp.conta_service_2025.dto.ContaResponseDTO;
+import com.bootcamp.conta_service_2025.exception.ContaExistenteException;
+import com.bootcamp.conta_service_2025.model.Conta;
+import com.bootcamp.conta_service_2025.repository.ContaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ContaService {
+    private final ContaRepository contaRepository;
+    public ContaResponseDTO criarConta(ContaRequestDTO contaRequestDTO){
+        Optional<Conta> contaOptional = contaRepository.findByNomeTitularAndNumeroContaAndChavePix(
+                contaRequestDTO.getNomeTitular(),
+                contaRequestDTO.getNumeroConta(),
+                contaRequestDTO.getChavePix()
+        );
+
+        if (contaOptional.isPresent()){
+            throw new ContaExistenteException("Conta já existe.");
+        }
+
+        Conta conta = Conta.builder()
+                .nomeTitular(contaRequestDTO.getNomeTitular())
+                .numeroAgencia(contaRequestDTO.getNumeroAgencia())
+                .numeroConta(contaRequestDTO.getNumeroConta())
+                .chavePix(contaRequestDTO.getChavePix())
+                .saldo(new BigDecimal(5000))
+                .build();
+
+        Conta contaSalva = contaRepository.save(conta);
+
+        ContaResponseDTO contaResponseDTO = ContaResponseDTO.builder()
+                .id(contaSalva.getId())
+                .nomeTitular(contaRequestDTO.getNomeTitular())
+                .build();
+
+        log.info("ContaResponseDTO: {}", contaResponseDTO);
+
+        return contaResponseDTO;
+    }
+}

3fe390332bd783f66580b73f3f22be54eb7d0802
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-04 20:49:37 -0300

    feat: create Controller file

---
+++ src/main/java/com/bootcamp/conta_service_2025/controller/ContaController.java
@@ -0,0 +1,45 @@
+package com.bootcamp.conta_service_2025.controller;
+
+import com.bootcamp.conta_service_2025.dto.ContaDTO;
+import com.bootcamp.conta_service_2025.dto.ContaRequestDTO;
+import com.bootcamp.conta_service_2025.dto.ContaResponseDTO;
+import com.bootcamp.conta_service_2025.service.ContaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/contas")
+@RequiredArgsConstructor
+public class ContaController {
+    private final ContaService contaService;
+
+    @PostMapping
+    public ResponseEntity<ContaResponseDTO> conta(@RequestBody @Valid ContaRequestDTO contaRequestDTO) throws Exception{
+        ContaResponseDTO contaResponseDTO = contaService.criarConta(contaRequestDTO);
+        return new ResponseEntity<>(contaResponseDTO, CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ContaResponseDTO> atualizarConta(@RequestBody @Valid ContaRequestDTO contaRequestDTO, @PathVariable UUID id) throws Exception{
+        ContaResponseDTO contaResponseDTO = contaService.atualizarConta(contaRequestDTO, id);
+        return ResponseEntity.status(HttpStatus.OK).body(contaResponseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContaDTO> conta(@PathVariable UUID id) throws Exception{
+        return ResponseEntity.status(HttpStatus.OK).body(contaService.buscarContaById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContaDTO>> contas(){
+        return ResponseEntity.status(HttpStatus.OK).body(contaService.buscaTodasContas());
+    }
+}

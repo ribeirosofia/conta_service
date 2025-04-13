17450abff1901382374ecc9d9c3749e40dcba0b0
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-13 19:46:57 -0300

    feat: create method findByIdempotencia

--- src/main/java/com/bootcamp/conta_service_2025/repository/PixRepository.java
+++ src/main/java/com/bootcamp/conta_service_2025/repository/PixRepository.java
@@ -3,9 +3,11 @@ package com.bootcamp.conta_service_2025.repository;
 import com.bootcamp.conta_service_2025.model.Pix;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PixRepository extends JpaRepository<Pix, UUID> {
 
+    Optional<Pix> findByIdempotencia(final String idempotencia);
 
 }

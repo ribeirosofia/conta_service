7d2a8e4abb950686a7e648dde6ac2f5951da4d63
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-13 19:47:15 -0300

    feat: create PixController

---
+++ src/main/java/com/bootcamp/conta_service_2025/controller/PixController.java
@@ -0,0 +1,28 @@
+package com.bootcamp.conta_service_2025.controller;
+
+import com.bootcamp.conta_service_2025.dto.PixRequestDTO;
+import com.bootcamp.conta_service_2025.dto.PixResponseDTO;
+import com.bootcamp.conta_service_2025.service.PixService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/pix")
+@RestController
+@RequiredArgsConstructor
+public class PixController {
+
+    private final PixService pixService;
+
+    @PostMapping
+    public ResponseEntity<PixResponseDTO> pix(@RequestBody @Valid PixRequestDTO pixRequestDTO){
+        PixResponseDTO pixResponseDTO = pixService.realizaPix(pixRequestDTO);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(pixResponseDTO);
+    }
+
+}

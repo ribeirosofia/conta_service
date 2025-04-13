db153988e4dd171a63d851a61bb3c3daa984e87f
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-13 20:24:51 -0300

    feat: insert RestControllerAdvice notation

--- src/main/java/com/bootcamp/conta_service_2025/configuration/ControllerExceptionHandler.java
+++ src/main/java/com/bootcamp/conta_service_2025/configuration/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -15,6 +16,7 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ContaExistenteException.class)
     private ProblemDetail exceptionContaExistente(ContaExistenteException ex){

1ba0ce090f98a33eada7e898f10778cf52fb8632
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-04-14 22:16:00 -0300

    feat: create ValorInvalidoException

---
+++ src/main/java/com/bootcamp/conta_service_2025/exception/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package com.bootcamp.conta_service_2025.exception;
+
+public class ValorInvalidoException extends RuntimeException {
+    public ValorInvalidoException(String message) {
+        super(message);
+    }
+}
